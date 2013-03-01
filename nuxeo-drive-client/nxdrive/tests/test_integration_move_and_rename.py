19de6b867d70678588f7a6d37351dbade2929d59
Michael Constantin <constantinm@sharplabs.com>
2013-03-01 11:43:49 -0800

    merged NXP-11127: changed the remote document client to not try to fetch parent.

--- nuxeo-drive-client/nxdrive/tests/test_integration_move_and_rename.py
+++ nuxeo-drive-client/nxdrive/tests/test_integration_move_and_rename.py
@@ -3,6 +3,11 @@ import time
 
 from nxdrive.tests.common import IntegrationTestCase
 from nxdrive.client import LocalClient
+<<<<<<< HEAD
+=======
+from nxdrive.client import RemoteFileSystemClient
+from nxdrive.client import RemoteDocumentClient
+>>>>>>> 6266e7c... NXP-11127: changed the remote document client to not try to fetch parent
 
 
 class TestIntegrationMoveAndRename(IntegrationTestCase):
@@ -90,8 +95,10 @@ class TestIntegrationMoveAndRename(IntegrationTestCase):
         self.assertEquals(file_1_remote_info.name,
             u'Renamed Again File 1.txt')
 
+        # User 1 does not have the rights to see the parent container
+        # of the test workspace, hence set fetch_parent_uid=False
         parent_of_file_1_remote_info = remote_client.get_info(
-            file_1_remote_info.parent_uid)
+            file_1_remote_info.parent_uid, fetch_parent_uid=False)
         self.assertEquals(parent_of_file_1_remote_info.name,
             self.workspace_title)
 
@@ -156,8 +163,169 @@ class TestIntegrationMoveAndRename(IntegrationTestCase):
         self.assertEquals(parent_of_file_1_remote_info.name,
             u'Original Folder 1')
 
+<<<<<<< HEAD
     # def test_local_rename_folder(self):
     #    pass
+=======
+        # Nothing left to do
+        time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
+        self.wait()
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 0)
+
+    def test_local_rename_folder(self):
+        sb, ctl = self.sb_1, self.controller_1
+        local_client = self.local_client_1
+        remote_client = self.remote_document_client_1
+
+        # Save the uid of some files and folders prior to renaming
+        original_folder_1_uid = remote_client.get_info(
+            u'/Original Folder 1').uid
+        original_file_1_1_uid = remote_client.get_info(
+            u'/Original Folder 1/Original File 1.1.txt').uid
+        original_sub_folder_1_1_uid = remote_client.get_info(
+            u'/Original Folder 1/Sub-Folder 1.1').uid
+
+        # Rename a non empty folder with some content
+        local_client.rename(u'/Original Folder 1', u'Renamed Folder 1 \xe9')
+        self.assertFalse(local_client.exists(u'/Original Folder 1'))
+        self.assertTrue(local_client.exists(u'/Renamed Folder 1 \xe9'))
+
+        # Synchronize: only the folder renaming is detected: all
+        # the descendants are automatically realigned
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 1)
+
+        # The server folder has been renamed: the uid stays the same
+        new_remote_name = remote_client.get_info(original_folder_1_uid).name
+        self.assertEquals(new_remote_name, u"Renamed Folder 1 \xe9")
+
+        # The content of the renamed folder is left unchanged
+        file_1_1_info = remote_client.get_info(original_file_1_1_uid)
+        self.assertEquals(file_1_1_info.name, u"Original File 1.1.txt")
+        self.assertEquals(file_1_1_info.parent_uid, original_folder_1_uid)
+
+        sub_folder_1_1_info = remote_client.get_info(
+            original_sub_folder_1_1_uid)
+        self.assertEquals(sub_folder_1_1_info.name, u"Sub-Folder 1.1")
+        self.assertEquals(sub_folder_1_1_info.parent_uid,
+            original_folder_1_uid)
+
+        # The more things change, the more they remain the same.
+        time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
+        self.wait()
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 0)
+
+    def test_local_move_folder(self):
+        sb, ctl = self.sb_1, self.controller_1
+        local_client = self.local_client_1
+        remote_client = self.remote_document_client_1
+
+        # Save the uid of some files and folders prior to move
+        original_folder_1_uid = remote_client.get_info(
+            u'/Original Folder 1').uid
+        original_folder_2_uid = remote_client.get_info(
+            u'/Original Folder 2').uid
+        original_file_1_1_uid = remote_client.get_info(
+            u'/Original Folder 1/Original File 1.1.txt').uid
+        original_sub_folder_1_1_uid = remote_client.get_info(
+            u'/Original Folder 1/Sub-Folder 1.1').uid
+
+        # Move a non empty folder with some content
+        local_client.move(u'/Original Folder 1', u'/Original Folder 2')
+        self.assertFalse(local_client.exists(u'/Original Folder 1'))
+        self.assertTrue(local_client.exists(
+            u'/Original Folder 2/Original Folder 1'))
+
+        # Synchronize: only the folder move is detected: all
+        # the descendants are automatically realigned
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 1)
+
+        # The server folder has been moved: the uid stays the same
+        remote_folder_info = remote_client.get_info(original_folder_1_uid)
+
+        # The parent folder is not folder 2
+        self.assertEquals(remote_folder_info.parent_uid,
+            original_folder_2_uid)
+
+        # The content of the renamed folder is left unchanged
+        file_1_1_info = remote_client.get_info(original_file_1_1_uid)
+        self.assertEquals(file_1_1_info.name, u"Original File 1.1.txt")
+        self.assertEquals(file_1_1_info.parent_uid, original_folder_1_uid)
+
+        sub_folder_1_1_info = remote_client.get_info(
+            original_sub_folder_1_1_uid)
+        self.assertEquals(sub_folder_1_1_info.name, u"Sub-Folder 1.1")
+        self.assertEquals(sub_folder_1_1_info.parent_uid,
+            original_folder_1_uid)
+
+        # The more things change, the more they remain the same.
+        time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
+        self.wait()
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 0)
+
+    def test_concurrent_local_rename_folder(self):
+        sb, ctl = self.sb_1, self.controller_1
+        local_client = self.local_client_1
+        remote_client = self.remote_document_client_1
+
+        # Save the uid of some files and folders prior to renaming
+        folder_1_uid = remote_client.get_info(u'/Original Folder 1').uid
+        file_1_1_uid = remote_client.get_info(
+            u'/Original Folder 1/Original File 1.1.txt').uid
+        folder_2_uid = remote_client.get_info(u'/Original Folder 2').uid
+        file_3_uid = remote_client.get_info(
+            u'/Original Folder 2/Original File 3.txt').uid
+
+        # Rename a non empty folders concurrently
+        local_client.rename(u'/Original Folder 1', u'Renamed Folder 1')
+        local_client.rename(u'/Original Folder 2', u'Renamed Folder 2')
+        self.assertFalse(local_client.exists(u'/Original Folder 1'))
+        self.assertTrue(local_client.exists(u'/Renamed Folder 1'))
+        self.assertFalse(local_client.exists(u'/Original Folder 2'))
+        self.assertTrue(local_client.exists(u'/Renamed Folder 2'))
+
+        # Synchronize: only the folder renamings are detected: all
+        # the descendants are automatically realigned
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 2)
+
+        # The server folders have been renamed: the uid stays the same
+        folder_1_info = remote_client.get_info(folder_1_uid)
+        self.assertEquals(folder_1_info.name, u"Renamed Folder 1")
+
+        folder_2_info = remote_client.get_info(folder_2_uid)
+        self.assertEquals(folder_2_info.name, u"Renamed Folder 2")
+
+        # The content of the folder has been left unchanged
+        file_1_1_info = remote_client.get_info(file_1_1_uid)
+        self.assertEquals(file_1_1_info.name, u"Original File 1.1.txt")
+        self.assertEquals(file_1_1_info.parent_uid, folder_1_uid)
+
+        file_3_info = remote_client.get_info(file_3_uid)
+        self.assertEquals(file_3_info.name, u"Original File 3.txt")
+        self.assertEquals(file_3_info.parent_uid, folder_2_uid)
+
+        # The more things change, the more they remain the same.
+        time.sleep(self.AUDIT_CHANGE_FINDER_TIME_RESOLUTION)
+        self.wait()
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 0)
+
+    def test_local_rename_sync_root_folder(self):
+        sb, ctl = self.sb_1, self.controller_1
+        # Use the Administrator to be able to introspect the container of the
+        # test workspace.
+        remote_client = RemoteDocumentClient(
+            self.nuxeo_url, self.admin_user,
+            'nxdrive-test-administrator-device',
+            self.password, base_folder=self.workspace)
+
+        folder_1_uid = remote_client.get_info(u'/Original Folder 1').uid
+
+        # Create new clients to be able to introspect the test sync root
+        toplevel_local_client = LocalClient(self.local_nxdrive_folder_1)
+
+        toplevel_local_client.rename('/' + self.workspace_title,
+            'Renamed Nuxeo Drive Test Workspace')
+        self.assertEquals(ctl.synchronizer.update_synchronize_server(sb), 1)
+>>>>>>> 6266e7c... NXP-11127: changed the remote document client to not try to fetch parent
 
     # def test_local_rename_sync_root_folder(self):
     #    pass
