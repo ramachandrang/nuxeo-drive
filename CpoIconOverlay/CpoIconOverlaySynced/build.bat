979060a71618d6bd28acbcb14bce1ff4df958a0e
Grant Windes <windesg@sharplabs.com>
2013-04-11 17:09:25 -0700

    Added build cleanup for deletion of unnecessary files after a release build

--- CpoIconOverlay/CpoIconOverlaySynced/build.bat
+++ CpoIconOverlay/CpoIconOverlaySynced/build.bat
@@ -1,7 +1,9 @@
 @echo off
-echo Building 32bit CpoIconSynced
 cd /d %~dp0
+echo Building 32bit CpoIconSynced
 devenv.exe CpoIconOverlaySynced.sln /project "CpoIconOverlaySynced.vcxproj" /Rebuild "Release|win32"
 echo Building 64bit CpoIconSynced
 devenv.exe CpoIconOverlaySynced.sln /project "CpoIconOverlaySynced.vcxproj" /Rebuild "Release|x64"
+del /q .\Release\*.*
+del /q .\x64\Release\*.*
 cd ..
